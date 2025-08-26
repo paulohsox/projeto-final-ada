4267ed41ce087174d46fceb1a8252fe873836c15
Paulo <paulohsox@gmail.com>
2025-08-25 21:53:58 -0300

    users in console.log

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet, Router } from '@angular/router';
-import { Navbar } from "./shared/navbar/navbar";
+import { Navbar } from './shared/navbar/navbar';
 import { AuthService } from './core/services/auth.service';
 import { ProductsService } from './core/services/products.service';
 import { SnackbarService } from './core/services/snackbar.service';
@@ -9,27 +9,37 @@ import { SnackbarService } from './core/services/snackbar.service';
   selector: 'app-root',
   imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App implements OnInit {
   private readonly router = inject(Router);
   private readonly authService = inject(AuthService);
   private readonly productsService = inject(ProductsService);
   private readonly snackBar = inject(SnackbarService);
-  
+
   protected isUserLoggedIn = this.authService.isUserLoggedIn;
   protected isUserAdmin = this.authService.isUserAdmin;
   protected readonly user = this.authService.user;
 
   protected title = 'vitrine-projeto-final';
 
-  onLogout(){
+  onLogout() {
     this.authService.logout();
     this.router.navigate(['/home']);
-    this.snackBar.show('Logged out successfully', 'info')
+    this.snackBar.show('Logged out successfully', 'info');
   }
 
   ngOnInit() {
     this.productsService.getAllProducts().subscribe();
+
+    console.log('*******************');
+    console.log('To test ADMIN:');
+    console.log('username = johnd');
+    console.log('password = m38rmF$');
+    console.log('*******************');
+    console.log('To test USER:');
+    console.log('username = kevinryan');
+    console.log('password = kev02937@');
+    console.log('*******************');
   }
 }
